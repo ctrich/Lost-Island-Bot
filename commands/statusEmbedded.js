39ad2e0744725bdcbb3c99d94de4ab16f96dfe5d
ctrich <ctrich86@yahoo.com>
2022-02-01 15:42:49 -0800

    removes number of days from embedded message and changed  thumbnail to lost island

--- commands/statusEmbedded.js
+++ commands/statusEmbedded.js
@@ -11,22 +11,23 @@ module.exports = {
             port: port,
             maxAttempts: 2
           }).then( state => {
+          console.log(state);
             const onlineEmbed = new Discord.MessageEmbed()
             .setColor('#009900')
-            .setTitle(`${state.map.replace("_P", "")} server stats`)
-            .setThumbnail('https://cdn.discordapp.com/attachments/718600522516529172/719030410553589781/maxresdefault.png')
+            .setTitle(`${state.name}`)
+            .setThumbnail('https://media.discordapp.net/attachments/719313280749011074/935141082537275423/unknown.png')
             .addFields(
                 { name: 'Server status', value: 'Online' },
                 { name: 'Players', value: `${state.players.length} of ${state.maxplayers}`},
-                { name: 'Day', value: state.raw.rules.DayTime_s, inline: true },
+                //{ name: 'Day', value: state.raw.rules.DayTime_s, inline: true },
             ).setTimestamp();
     
-            msg.channel.send(onlineEmbed);
+            msg.channel.send({ embeds: [onlineEmbed] });
           }).catch( error => {
             const offlineEmbed = new Discord.MessageEmbed()
             .setColor('#009900')
             .setTitle(`Lost Island server stats`)
-            .setThumbnail('https://cdn.discordapp.com/attachments/718600522516529172/719030410553589781/maxresdefault.png')
+            .setThumbnail('https://media.discordapp.net/attachments/719313280749011074/935141082537275423/unknown.png')
             .addField('Server status', 'Offline')
             .setTimestamp();
 
