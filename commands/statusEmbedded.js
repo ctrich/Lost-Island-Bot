a035291dbb08e25c897c71a3bdbc552ec1e60b60
ctrich <ctrich86@yahoo.com>
2022-06-26 14:02:24 -0700

    testing

--- commands/statusEmbedded.js
+++ commands/statusEmbedded.js
@@ -11,9 +11,9 @@ module.exports = {
             port: port,
             maxAttempts: 2
           }).then( state => {
-            const names = state.players;
+            const names = state.players.reduce(player => player.name);;
             console.log(state);
-            console.log("Player names " + names[0].name);
+            console.log("Player names " + names);
             const onlineEmbed = new Discord.MessageEmbed()
             .setColor('#009900')
             .setTitle(`${state.name}`)
